3975ea9ccd722881ee2cdfc18a3529e81fe920f1
Rangani Krushank <s220090107166@ckpcet.ac.in>
2026-03-09 15:35:21 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///instance/inventory.db"
+        "sqlite:////tmp/inventory.db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
