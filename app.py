bf0f2458b8aeaa6218c903e7997b151ce06967b5
Rangani Krushank <s220090107166@ckpcet.ac.in>
2026-04-27 20:33:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9419,8 +9419,7 @@ def create_app():
                 SchoolStockRequest.query
                 .filter(
                     SchoolStockRequest.seller_id == user.seller_id,
-                    SchoolStockRequest.request_type == "SCHOOL_TO_SELLER",
-                    SchoolStockRequest.status.in_(["SHIPPED", "RECEIVED"])  # ✅ ADD THIS ONLY
+                    SchoolStockRequest.request_type == "SCHOOL_TO_SELLER"
                 )
                 .order_by(SchoolStockRequest.id.desc())  # ✅ change ONLY this line
                 .all()
